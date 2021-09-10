f0785682588006bf3b5c9fb220048931c3d7ffe3
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-10 08:10:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -117,9 +117,16 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
 });
 
 app.get('/api/users/:_id/logs', async (req, res) => {
-  let { from, to, limit } = req.query;
+  let { limit = -1 } = req.query;
   let { _id } = req.params;
   let inDatabase;
+
+  let from = req.query.from
+    ? new Date(req.query.from).getTime()
+    : new Date('1-1-1').getTime();
+  let to = req.query.to
+    ? new Date(req.query.to).getTime()
+    : new Date().getTime();
 
   try {
     inDatabase = await UserModel.findById(_id);

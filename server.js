1de3da9b10cc006c3b341f713b6e71bc8c2f84fb
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-10 06:44:27 -0800

    Update server.js

--- server.js
+++ server.js
@@ -139,16 +139,13 @@ app.get('/api/users/:_id/logs', async (req, res) => {
         username: 'fcc_test',
         count: 0,
         _id: '5fb5853f734231456ccb3b05',
-        log: inDatabase
-          ? [...inDatabase]
-          : // [
-            //     {
-            //       description: 'test',
-            //       duration: 60,
-            //       date: 'Mon Jan 01 1990',
-            //     },
-            //   ]
-            [],
+        log: [
+          {
+            description: 'test',
+            duration: 60,
+            date: 'Mon Jan 01 1990',
+          },
+        ],
       });
 
       // ExerciseModel.find({ _id })

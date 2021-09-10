a5b335a18685f30d6e4d9b2173719a2f11ede5e2
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-09 23:06:01 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,23 +11,8 @@ app.use(express.static('public'));
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 
-function randomId() {
-  let randomStr = '8123143172839271934238793743';
-  let subStr = '';
-
-  for (let i = 0; i < 13; i++) {
-    subStr += randomStr[Math.floor(Math.random() * randomStr.length)];
-  }
-
-  return subStr * 1;
-}
-
 ///////////////models///////////////
 const UserSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: randomId(),
-  },
   username: {
     type: String,
   },
@@ -70,69 +55,69 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
-app.post('/api/users', (req, res) => {
-  const { username: reqUsername } = req.body;
-
-  // search for a document matching the username
-  UserModel.findOne(
-    {
-      username: reqUsername,
-    },
-    (errFound, userFound) => {
-      if (errFound) {
-        console.log('findOne() error');
-      }
-      // findOne() returns either **null** or a **document** matching the search
-      if (userFound) {
-        // if a document is found, return a message detailing how the name is not available
-        res.send('username already taken');
-      } else {
-        // else create a document for the input username
-        // detail log as an empty array
-        const user = new UserModel({
-          username: reqUsername,
-          log: [],
-        });
-        // save the object in the database
-        user.save((errSaved, userSaved) => {
-          if (errSaved) {
-            console.log('save() error');
-          }
-          // display a JSON object detailing the _id and the username
-          const { _id, username } = userSaved;
-          res.json({
-            username,
-            _id,
-          });
-        });
-      }
-    }
-  );
-});
-
-// app.post('/api/users', async (req, res) => {
+// app.post('/api/users', (req, res) => {
 //   const { username } = req.body;
-//   let inDatabase;
 
-//   try {
-//     inDatabase = await UserModel.findOne({ username });
-//     if (inDatabase) {
-//       throw new Error('user exists with username');
-//     } else {
-//       inDatabase = new UserModel({ username });
-//       await inDatabase.save();
-
-//       console.log(inDatabase);
-
-//       res.json({
-//         username: inDatabase.username,
-//         _id: inDatabase._id,
-//       });
+//   // search for a document matching the username
+//   UserModel.findOne(
+//     {
+//       username,
+//     },
+//     (errFound, userFound) => {
+//       if (errFound) {
+//         console.log('findOne() error');
+//       }
+//       // findOne() returns either **null** or a **document** matching the search
+//       if (userFound) {
+//         // if a document is found, return a message detailing how the name is not available
+//         res.send('username already taken');
+//       } else {
+//         // else create a document for the input username
+//         // detail log as an empty array
+//         const user = new UserModel({
+//           username,
+//         });
+//         // save the object in the database
+//         user.save((errSaved, userSaved) => {
+//           if (errSaved) {
+//             console.log('save() error');
+//           }
+//           // display a JSON object detailing the _id and the username
+//           const { _id, username } = userSaved;
+//           res.json({
+//             username,
+//             _id,
+//           });
+//         });
+//       }
 //     }
-//   } catch (error) {
-//     console.log(error);
-//   }
+//   );
 // });
+
+app.post('/api/users', async (req, res) => {
+  const { username } = req.body;
+  let inDatabase;
+
+  try {
+    inDatabase = await UserModel.findOne({ username });
+    if (inDatabase) {
+      throw new Error('user exists with username');
+    } else {
+      inDatabase = new UserModel({ username });
+      await inDatabase.save();
+
+      console.log('inDatabase');
+      console.log(inDatabase);
+
+      res.send({
+        username: inDatabase.username,
+        _id: inDatabase._id,
+      });
+    }
+  } catch (error) {
+    res.json({ error: 'something went wrong' });
+  }
+});
 
 app.post('/api/users/:userId?/exercises', async (req, res) => {
   let { userId, description, duration, date } = req.body;

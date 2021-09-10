fdf295cf0f627fbf5cf75f93104a771fdc999c5b
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-10 06:46:03 -0800

    Update server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/api/users/:_id/logs', async (req, res) => {
 
       res.json({
         username: 'fcc_test',
-        count: 0,
+        count: 1,
         _id: '5fb5853f734231456ccb3b05',
         log: [
           {

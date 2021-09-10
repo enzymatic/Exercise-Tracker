74deccf1098677c57d076e081879e5f0ff6447ca
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-09 22:56:47 -0800

    server.js update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const mongodb = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -71,29 +70,69 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
-app.post('/api/users', async (req, res) => {
-  const { username } = req.body;
-  let inDatabase;
+app.post('/api/users', (req, res) => {
+  const { username: reqUsername } = req.body;
 
-  try {
-    inDatabase = await UserModel.findOne({ username });
-    if (inDatabase) {
-      throw new Error('user exists with username');
-    } else {
-      inDatabase = new UserModel({ username });
-      await inDatabase.save();
-
-      console.log(inDatabase);
-
-      res.json({
-        username: inDatabase.username,
-        _id: inDatabase._id,
-      });
+  // search for a document matching the username
+  UserModel.findOne(
+    {
+      username: reqUsername,
+    },
+    (errFound, userFound) => {
+      if (errFound) {
+        console.log('findOne() error');
+      }
+      // findOne() returns either **null** or a **document** matching the search
+      if (userFound) {
+        // if a document is found, return a message detailing how the name is not available
+        res.send('username already taken');
+      } else {
+        // else create a document for the input username
+        // detail log as an empty array
+        const user = new UserModel({
+          username: reqUsername,
+          log: [],
+        });
+        // save the object in the database
+        user.save((errSaved, userSaved) => {
+          if (errSaved) {
+            console.log('save() error');
+          }
+          // display a JSON object detailing the _id and the username
+          const { _id, username } = userSaved;
+          res.json({
+            username,
+            _id,
+          });
+        });
+      }
     }
-  } catch (error) {
-    console.log(error);
-  }
+  );
 });
+
+// app.post('/api/users', async (req, res) => {
+//   const { username } = req.body;
+//   let inDatabase;
+
+//   try {
+//     inDatabase = await UserModel.findOne({ username });
+//     if (inDatabase) {
+//       throw new Error('user exists with username');
+//     } else {
+//       inDatabase = new UserModel({ username });
+//       await inDatabase.save();
+
+//       console.log(inDatabase);
+
+//       res.json({
+//         username: inDatabase.username,
+//         _id: inDatabase._id,
+//       });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 app.post('/api/users/:userId?/exercises', async (req, res) => {
   let { userId, description, duration, date } = req.body;

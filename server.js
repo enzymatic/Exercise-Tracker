416153a0cb4deb76457abff2fe6b3a4687eae182
enzymatic <89150482+enzymatic@users.noreply.github.com>
2021-09-10 05:27:02 -0800

    Update server.js

--- server.js
+++ server.js
@@ -116,99 +116,35 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
   }
 });
 
-app.get('/api/users/:_id/logs', (req, res) => {
-  const userId = req.params._id,
-    { limit } = req.query;
-  var from = req.query.from
-    ? new Date(req.query.from).getTime()
-    : new Date('1111-11-11').getTime();
-  var to = req.query.to
-    ? new Date(req.query.to).getTime()
-    : new Date().getTime();
+app.get('/api/users/:id/logs', async (req, res) => {
+  let { _id, from, to, limit } = req.query;
+  let inDatabase;
 
-  UserModel.findById(userId, (err, data) => {
-    if (err) console.error(err);
+  try {
+    inDatabase = await UserModel.findById(_id);
 
-    if (!data) {
-      res.send('Unknown userId');
+    if (!inDatabase) {
+      throw new Error('wrong id');
     } else {
-      const username = data.username;
-      console.log('*************************************');
-      console.log('USER LOG SEARCHED: ' + username);
-      console.log('from: ' + from, 'to: ' + to);
+      inDatabase = await ExerciseModel.findById(_id)
+        .where('date')
+        .gte(from)
+        .lte(to)
+        .limit(limit);
 
-      ExerciseModel.find(
-        { userId: userId } /*, {"date": {$gte: from, $lte: to}}*/
-      )
-        .select(['description', 'date', 'duration'])
-        .limit(+limit)
-        .sort({ date: -1 })
-        .exec((err, data) => {
-          if (err) console.error(err);
-          let count = 0;
-          let customData = data
-            .filter((element) => {
-              let newEle = new Date(element.date).getTime();
-              if (newEle >= from && newEle <= to) count++;
-              return newEle >= from && newEle <= to;
-            })
-            .map((element) => {
-              let newDate = new Date(element.date).toDateString();
-              return {
-                description: element.description,
-                duration: element.duration,
-                date: newDate,
-              };
-            });
-          if (!data) {
-            res.json({
-              _id: userId,
-              username: username,
-              count: 0,
-              log: [],
-            });
-          } else {
-            res.json({
-              _id: userId,
-              username: username,
-              count: count,
-              log: customData,
-            });
-          }
-        });
+      res.json({
+        _id,
+        log: inDatabase.map((item) => ({
+          description: item.description,
+          duration: item.duration,
+          date: item.date,
+        })),
+      });
     }
-  });
+  } catch (error) {
+    res.json({ error: 'something went wrong' });
+  }
 });
-
-// app.get('/api/users/:id/logs', async (req, res) => {
-//   let { _id, from, to, limit } = req.query;
-//   let inDatabase;
-
-//   try {
-//     inDatabase = await UserModel.findById(_id);
-
-//     if (!inDatabase) {
-//       throw new Error('wrong id');
-//     } else {
-//       inDatabase = await ExerciseModel.findById(_id)
-//         .where('date')
-//         .gte(from)
-//         .lte(to)
-//         .limit(limit);
-
-//       res.json({
-//         _id,
-//         log: inDatabase.map((item) => ({
-//           description: item.description,
-//           duration: item.duration,
-//           date: item.date,
-//         })),
-//       });
-//     }
-//   } catch (error) {
-//     res.json({ error: 'something went wrong' });
-//   }
-// });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
